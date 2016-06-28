a967e3fd497ac1fd3ad92eb3aaed62ca8c2f1791
hanaum <hanau12@gmail.com>
2016-06-27 22:45:14 -0700

    Finish queue using stacks.

---
+++ queueStacks.js
@@ -0,0 +1,37 @@
+function Queue() {
+	this.stack = [];
+}
+
+Queue.prototype.dequeue = function() {
+	if (this.stack.length === 0) {
+		return undefined;
+	}
+
+	var newStack = [];
+	var stackLength = this.stack.length;
+	for (var i = 0; i < stackLength - 1; i++) {
+		newStack.push(this.stack.pop());
+	}
+	var dequeued = this.stack.pop();
+	for (var i = 0; i < stackLength - 1; i++) {
+		this.stack.push(newStack.pop());
+	}
+	return dequeued;
+}
+
+Queue.prototype.enqueue = function(item) {
+	this.stack.push(item);
+}
+
+var queue = new Queue();
+queue.dequeue();
+queue.enqueue(6);
+queue.enqueue(5);
+queue.enqueue(4);
+queue.enqueue(3);
+console.log(queue);
+queue.dequeue();
+queue.enqueue(-1);
+console.log(queue);
+queue.dequeue();
+console.log(queue);
