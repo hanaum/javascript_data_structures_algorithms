543e40d7b6ac222aef1dd748101a82615b899bb4
hanaum <hanau12@gmail.com>
2016-06-27 22:39:02 -0700

    Finish findMinRotated

---
+++ findMinRotated.js
@@ -0,0 +1,23 @@
+function findMinRotated(a) {
+	var low = 0;
+	var high = a.length - 1;
+
+	while (a[low] > a[high] && (high - low) > 1) {
+		var mid = Math.floor((low + high) / 2);
+		if (a[mid] > a[low]) {
+			low = mid + 1;
+		} else {
+			low = low + 1;
+			high = mid;
+		}
+	}
+	return a[low];
+}
+
+
+var a = [16,17,18,1,2,3,5,6,7,8];
+console.log(findMinRotated(a));
+a = [1, 2, 3, 4, 5];
+console.log(findMinRotated(a));
+a = [1, 2, 3, 4, 5, 0];
+console.log(findMinRotated(a));
